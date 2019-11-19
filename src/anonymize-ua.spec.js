2f9ecdf4a2f20636c8ff16a4768b8becc11c29ce
Michael Johansen <michael.johansen@nav.no>
2019-11-19 12:39:43 +0100

    POC sending data (#2)

--- src/anonymize-ua.spec.js
+++ src/anonymize-ua.spec.js
@@ -6,6 +6,10 @@ describe('anonymize ua', async () => {
       ua: 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36',
       expected: 'Mozilla/redacted (Windows;U;Windows redacted;redacted) Chrome/redacted Blink/redacted',
     },
+    {
+      ua: 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.85 Safari/537.36',
+      expected: 'Mozilla/redacted (Windows;U;Windows redacted;redacted) Chrome/redacted Blink/redacted',
+    },
   ];
   params.forEach(function(param) {
     it('test that string is working', function() {
